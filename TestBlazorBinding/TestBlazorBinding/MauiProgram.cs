7da709fdae1cd04244f9ac9d0f37906adf18268c
gongbaodd <gongbaodd@qq.com>
2023-05-02 14:33:00 +0800

    MAUI blazor

---
+++ TestBlazorBinding/TestBlazorBinding/MauiProgram.cs
@@ -0,0 +1,20 @@
+﻿using BlazorBindings.Maui;
+
+namespace TestBlazorBinding;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<BlazorBindingsApplication<AppShell>>()
+            .UseMauiBlazorBindings()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        return builder.Build();
+    }
+}
---
+++ TestBlazorBinding/TestBlazorBinding/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TestBlazorBinding;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
