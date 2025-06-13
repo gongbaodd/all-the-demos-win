af436aaf3b34f27009e7360f3fd6928cd61121dd
gongbaodd@ProArt <gongbaodd@qq.com>
2025-06-13 17:18:06 +0300

    tank moves

--- tank-ecs/src/scripts/tankCar.ts
+++ tank-ecs/src/scripts/tankCar.ts
@@ -1,9 +1,11 @@
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
 export function onStart(mesh: Mesh): void {
+    console.log(mesh)
     // Do something when the script is loaded
 }
 
 export function onUpdate(mesh: Mesh): void {
+    // console.log(mesh.getScene().getAnimationRatio())
     mesh.rotation.y += 0.04 * mesh.getScene().getAnimationRatio();
 }
